ca3570529f5975f34183b2510d156153d1e24426
barberj <barber.justin@gmail.com>
2014-09-27 20:35:12 -0400

    mandrill

--- app.py
+++ app.py
@@ -1,25 +1,58 @@
-import os
-import pdb
+import os, cStringIO, mandrill, base64, time
 from flask import Flask, render_template, request, redirect, url_for
+
 app = Flask(__name__)
+
+def send_email(subject, fields, file_to_attach=None):
+    if file_to_attach:
+        attachment = {
+            'content': base64.b64encode(file_to_attach.read()),
+            'name': file_to_attach.filename,
+            'type': file_to_attach.content_type
+        }
+
+    text = ''
+    for k in fields:
+        text += "%s: %s\n" % (k, fields[k])
+
+    message = {
+        'from_email': 'no-reply@pairstaffing.com',
+        'from_name': 'Pair Staffing',
+        'headers': { 'Reply-To': 'no-reply@pairstaffing.com' },
+        'html': text,
+        'important': True,
+        'subject': subject,
+        'text': text,
+        'to': [{
+            'email': 'info@pairstaffing.com',
+            'name': 'Evan Myers',
+            'type': 'to'
+        }]
+    }
+    if file_to_attach:
+        message['attachments'] = [attachment]
+
+    mandrill.Mandrill().messages.send(message=message, async=False)
+
+
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route("/candidate")
+@app.route("/candidate", methods=['POST'])
 def candidate():
-    for item in request.form:
-        print item
-    pdb.set_trace()
+    send_email('Candidate Resume', request.form, request.files['file'])
     return redirect(url_for('index'))
 
 @app.route("/employer")
 def employer():
+    send_email('Job Details', request.form, request.files['file'])
     return redirect(url_for('index'))
 
 @app.route("/other")
 def other():
+    send_email('Comment/Question', request.form)
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
--- static/custom.js
+++ static/custom.js
@@ -6,6 +6,6 @@ $(document).ready(function() {
     $('#contact a[href="#' + this.dataset['tab'] + '"]').tab('show');
   });
   $('form button').click(function (event){
-    $('#' + this.classList[this.classList.length-1]).submit();
+    $('#' + this.classList[this.classList.length-1] + '_form').submit();
   });
 });
